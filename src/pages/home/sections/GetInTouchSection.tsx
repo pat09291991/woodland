3865fc924fd16a348206ce64c091b94645473767
Patrick <patrick@voma.ai>
2025-09-14 11:21:53 +0800

    modify tablet views

--- src/pages/home/sections/GetInTouchSection.tsx
+++ src/pages/home/sections/GetInTouchSection.tsx
@@ -108,7 +108,7 @@ const GetInTouchSection: React.FC = () => {
               <div className="rounded-lg h-48 overflow-hidden">
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3869.4813926734205!2d121.41464557596117!3d14.1077637863223!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5a43f5018d99%3A0xe6499bd1432cfe89!2sWoodland%20Villa!5e0!3m2!1sen!2sph!4v1757817238360!5m2!1sen!2sph"
-                  width="600"
+                  width="100%"
                   height="450"
                   style={{ border: 0 }}
                   allowFullScreen
